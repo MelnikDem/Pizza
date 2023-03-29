8a549d414a16c71c926522e957873e8d2a4372c3
git <dima.it.ua@gmail.com>
2023-03-29 23:43:22 +0300

    night mode finish

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,3 +1,4 @@
+
 const modeBtn = document.querySelector(".button-mode");
 modeBtn.addEventListener("click", () => {
   document.body.classList.toggle("night");
@@ -15,12 +16,20 @@ modeBtn.addEventListener("click", () => {
     document.querySelector(".contacts__btn").style.color = "#151515";
     document.querySelector(".contacts__btn").style.backgroundColor = "#FF5B00";
     document.querySelector(".contacts__heading").classList.toggle("night-orange")
-  }
+  };
 
   const svgIcon = document.querySelector("#svg-icon");
-    if (modeBtn.classList.contains("night")) {
-      svgIcon.setAttribute("href", './img/symbol-defs.svg#icon-Cloud_Sun');
-    } else {
-      svgIcon.setAttribute("href", './img/symbol-defs.svg#icon-darkmode');
-    }
-  })
+  if (modeBtn.classList.contains("night")) {
+    svgIcon.setAttribute("href", './img/symbol-defs.svg#icon-Cloud_Sun');
+  } else {
+    svgIcon.setAttribute("href", './img/symbol-defs.svg#icon-darkmode');
+  };
+  
+  accordionItem.forEach(Element => Element.classList.toggle(".night-accordion"));
+  document.querySelector(".order__account").classList.toggle("active");
+  document.querySelector(".order__address").classList.toggle("active");
+  document.querySelector(".order__payment").classList.toggle("active");
+  document.querySelector(".form__block").classList.toggle("night-form");
+  document.querySelector(".form__header").classList.toggle("night-orange");
+
+});
